c564ecbd13ee65cb9b511d191a357404b72a0ef5
Jesus Serrano <jesus.serrano@gmail.com>
2019-08-18 23:20:11 +0200

    fill basic readme file

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # wp-docker
+
+wp-docker is a command line scripts that helps developers to create and manage WordPress projects in local docker containers easily.
+
+```
+Usage wp-docker <command> [<options>]
+
+Commands:
+  create                         Create new wp-docker environment
+  list                           Show available environments
+  cli <command>                  Execute wp-cli command
+  open                           Open browser
+  shell                          Open shell in web container
+  exec <command>                 Execute command inside container
+  start <environment>            Start wp-docker environment
+  stop <environment>             Stop wp-docker environment
+  remove <environment>           Remove wp-docker environment
+  logs [-t]                      Show apache web logs
+  help                           Show this help
+```
+
+
+## Contributing
+
+* If you have found a bug or if you have a feature request, please report them at this repository issues section.
+* Pull requests must be made against develop branch.
--- lib/commands/help.rb
+++ lib/commands/help.rb
@@ -1,17 +1,19 @@
 def wp_docker_help
-    puts "Usage wp-docker <command> [<options>]"
-    puts 
-    puts "Commands:"        
-    puts "  create                         Create new wp-docker environment"
-    puts "  list                           Show available environments"
-    puts "  cli <command>                  Execute wp-cli in current wp-docker environment"
-    puts "  open                           Open browser"
-    puts "  shell                          Open shell in web container"
-    puts "  exec <command>                 Execute command in current wp-docker environment"
-    puts "  start <environment>            Start wp-docker environment"
-    puts "  stop <environment>             Stop wp-docker environment"
-    puts "  remove <environment>           Remove wp-docker environment"
-    puts "  logs [-t]                      Show apache web logs"
-    puts "  help                           Show this help"
-    puts
+puts """
+Usage wp-docker <command> [<options>]
+
+Commands:
+  create                         Create new wp-docker environment
+  list                           Show available environments
+  cli <command>                  Execute wp-cli in current wp-docker environment
+  open                           Open browser
+  shell                          Open shell in web container
+  exec <command>                 Execute command in current wp-docker environment
+  start <environment>            Start wp-docker environment
+  stop <environment>             Stop wp-docker environment
+  remove <environment>           Remove wp-docker environment
+  logs [-t]                      Show apache web logs
+  help                           Show this help
+  
+"""
 end
