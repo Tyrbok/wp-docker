556ff5240622e12490c3c30256ba0beff16a7744
Jesus Serrano <jesus.serrano@gmail.com>
2019-08-18 19:59:01 +0200

    Show web and db ports in environment list

--- lib/docker_manager.rb
+++ lib/docker_manager.rb
@@ -1,27 +1,39 @@
 require 'open3'
 
 class DockerManager
-
+    
     def get_environments
         command = "docker ps --all"
         stdout, stderr, status = Open3.capture3(command)
+        
+        match_envs = stdout.scan(/wpd_web_(.+_\d+)/)
+        match_web = stdout.scan(/.*0\.0\.0\.0:(\d+)->80\/tcp.*wpd_web_(.+_\d+)/)
+        match_db = stdout.scan(/.*0\.0\.0\.0:(\d+)->3306\/tcp.*wpd_db_(.+_\d+)/)
+        
+        match = Array.new
+        match_envs.each do |env|
 
-        match = stdout.scan(/wpd_web_(.+_\d+)/).map { |item| item[0] }
+            web_port = match_web.select { |item| item[1] == env[0] }        
+            db_port = match_db.select { |item| item[1] == env[0] }        
+
+            match.push({"env" => env[0], "web_port" => web_port[0][0], "db_port" => db_port[0][0]})
+        end
+        
         return match
     end 
-
+    
     def get_new_wpd_id
         command = "docker ps --all"
         stdout, stderr, status = Open3.capture3(command)
-
+        
         match = stdout.scan(/wpd_.+_(\d+)/)
         current_ids = match.uniq
-
+        
         if current_ids.count > 89
             puts "No more available ids"
             exit
         end
-
+        
         new_id = nil
         while new_id == nil
             rand_id = rand(10..99)
@@ -29,27 +41,27 @@ class DockerManager
                 new_id = rand_id
             end
         end
-
+        
         return new_id
     end
-
+    
     def start environment
         web = "wpd_web_" + environment
         db = "wpd_db_" + environment
-
+        
         puts "Start: " + db
         command="docker start " + db
         system(command)
-
+        
         puts "Start: " + web
         command="docker start " + web
         system(command)        
     end
-
+    
     def stop environment
         web = "wpd_web_" + environment
         db = "wpd_db_" + environment
-
+        
         puts "Stop: " + web
         command="docker stop " + web
         system(command)
@@ -58,18 +70,18 @@ class DockerManager
         command="docker stop " + db
         system(command)
     end
-
+    
     def remove environment
-
+        
         self.stop environment
-
+        
         web = "wpd_web_" + environment
         db = "wpd_db_" + environment
-
+        
         puts "Remove: " + web
         command="docker rm " + web
         system(command)
-
+        
         puts "Remove: " + db
         command="docker rm " + db
         system(command)
